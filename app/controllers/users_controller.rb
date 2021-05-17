409de1f299668d6f31996e5535364225dbff8c22
EC2 Default User <ec2-user@ip-172-31-41-70.us-east-2.compute.internal>
2021-05-17 11:16:07 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,37 +6,38 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # ページングのpageメソッドを利用して、すべての投稿画像が表示されないように設定
     @book = Book.new
-    
+
+
   end
-  
+
   def after_log_in_path_for(resource)
     flash[:success] = "Signed in successfully."
     user_path(@user.id)
   end
-  
-  
+
+
 
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
   end
-  
+
   def edit
     @user = User.find(params[:id])
     if @user.id != current_user.id
-    redirect_to users_path
+    redirect_to users_path(@user)
     end
   end
 
   def update
     @user = User.find(params[:id])
     if@user.update(user_params)
-      flash[:notice] = "You have updated user successfully." 
+      flash[:notice] = "You have updated user successfully."
     redirect_to  user_path(params[:id])
     else
     render 'edit'
-    end 
+    end
   end
 
   # def limitation_correct_user
@@ -50,7 +51,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def ensure_correct_user
     @user = User.find_by(id: params[:id])
     if @user.id != current_user.id
